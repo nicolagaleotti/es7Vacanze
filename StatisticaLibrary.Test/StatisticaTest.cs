64a25ea6cd76ce7f0d655e88a2b260fb8727f581
Nicola Galeotti <niccio2802@outlook.it>
2020-08-12 11:41:23 +0200

    fine

--- StatisticaLibrary.Test/StatisticaTest.cs
+++ StatisticaLibrary.Test/StatisticaTest.cs
@@ -7,8 +7,54 @@ namespace StatisticaLibrary.Test
     public class StatisticaTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIsPositivo()
         {
+            int n = 2;
+            bool resp = Statistica.IsPositivo(n);
+            bool asp = true;
+            Assert.AreEqual(asp, resp);
+        }
+
+        [DataTestMethod]
+        [DataRow(2, true)]
+        public void DataTestIsPositivo(int n, bool asp)
+        {
+            bool resp = Statistica.IsPositivo(n);
+            Assert.AreEqual(asp, resp);
+        }
+
+        [TestMethod]
+        public void TestContaPositivi()
+        {
+            int[] n = new int[] { 1, -2, 6, -3 };
+            int resp = Statistica.ContaPositivi(n);
+            int asp = 2;
+            Assert.AreEqual(asp, resp);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, -2, 6, -3 }, 2)]
+        public void DataTestContaPositivi(int[] n, int asp)
+        {
+            int resp = Statistica.ContaPositivi(n);
+            Assert.AreEqual(asp, resp);
+        }
+
+        [TestMethod]
+        public void TestPositivi()
+        {
+            int[] n = new int[] { 1, -2, 6, -3 };
+            int[] resp = Statistica.Positivi(n);
+            int[] asp = new int[] { 1, 6 };
+            CollectionAssert.AreEquivalent(asp, resp);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, -2, 6, -3 }, new int[] { 1, 6 })]
+        public void DataTestPositivi(int[] n, int[] asp)
+        {
+            int[] resp = Statistica.Positivi(n);
+            CollectionAssert.AreEquivalent(asp, resp);
         }
     }
 }
