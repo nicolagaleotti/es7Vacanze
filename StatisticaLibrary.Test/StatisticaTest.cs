21313e76dade604dc7dcbb4c79e72cbb837e9b26
Nicola Galeotti <niccio2802@outlook.it>
2020-08-12 11:29:21 +0200

    Add project files.

---
+++ StatisticaLibrary.Test/StatisticaTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatisticaLibrary;
+
+namespace StatisticaLibrary.Test
+{
+    [TestClass]
+    public class StatisticaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ StatisticaLibrary/Statistica.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StatisticaLibrary
+{
+    public class Statistica
+    {
+        public static bool IsPositivo(int n)
+        {
+            bool resp = false;
+            if (n > 0)
+                resp = true;
+            return resp;
+        }
+        
+        public static int ContaPositivi(int[] num)
+        {
+            int c = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > 0)
+                    c++;
+            }
+            return c;
+        }
+
+        public static int[] Positivi(int[] num)
+        {
+            List<int> positivi = new List<int>();
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > 0)
+                    positivi.Add(num[i]);
+            }
+            int[] resp = new int[positivi.Count];
+            for (int i = 0; i < resp.Length; i++)
+            {
+                resp[i] = positivi[i];
+            }
+            return resp;
+        }
+    }
+}
